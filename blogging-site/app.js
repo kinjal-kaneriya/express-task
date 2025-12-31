806178de21fe84b7ea77555f86c56255725d43e1
kinjal-kaneriya <kinjalk.encircle@gmail.com>
2025-12-31 12:12:26 +0530

    style: tailwind css added

--- blogging-site/app.js
+++ blogging-site/app.js
@@ -1,53 +1,85 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-app.use(express.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 let blogs = [
   {
     id: 1,
-    title: "First Blog Post",
-    author: "Admin",
-    content: "This is my first static blog post using Node and EJS."
+    title: 'Margherita Pizza',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP0HbRY0SsECXq3XHqjXUBw3CqK1VfE5PX1w&s',
+    content:
+      'Margherita pizza is a classic Italian pizza made with fresh tomatoes, mozzarella cheese, basil, and olive oil. It has a simple and fresh taste.',
   },
   {
     id: 2,
-    title: "Learning Express",
-    author: "Kinjal",
-    content: "Express makes backend development simple and fast."
+    title: 'Pepperoni Pizza',
+    image:
+      'https://www.simplyrecipes.com/thmb/KE6iMblr3R2Db6oE8HdyVsFSj2A=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-3-1024x682-583b275444104ef189d693a64df625da.jpg',
+    content:
+      'Pepperoni pizza is one of the most popular pizza varieties. It is topped with spicy pepperoni slices, mozzarella cheese, and tomato sauce.',
+  },
+]
+
+// Home Page
+app.get('/', (req, res) => {
+  res.render('index', { blogs })
+})
+
+//Create new blog
+app.post('/newblog', (req, res) => {
+  const { title, image, content } = req.body
+
+  blogs.push({
+    id: blogs.length + 1,
+    title,
+    image,
+    content,
+  })
+
+  res.redirect('/blogList')
+})
+
+//Blog list
+app.get('/blogList', (req, res) => {
+  res.render('blogList', { blogs })
+})
+
+//Edit page
+app.get('/edit/:i', (req, res) => {
+  const index = req.params.i
+  const blog = blogs[index]
+
+  if (blog) {
+    res.render('editBlog', { blog, index })
+  } else {
+    res.send('Blog not found')
   }
-];
-
-app.get("/", (req, res) => {
-    res.render("index", {blogs});
 })
 
-app.post("/newblog", (req, res) => {
-    const {title, author, content} = req.body;
+// Update blog
+app.post('/edit/:index', (req, res) => {
+  const index = req.params.index
+  const { title, image, content } = req.body
 
-    blogs.push({
-        id: blogs.length + 1,
-        title, 
-        author,
-        content
-    })
-
-    res.redirect("/blogList");
+  if (blogs[index]) {
+    blogs[index] = { ...blogs[index], title, image, content }
+    res.redirect('/blogList')
+  } else {
+    res.send('Blog Not found')
+  }
 })
 
-app.get("/blogList", (req, res) => {
-    res.render("blogList", {blogs});
-})
-
-app.post("/delete/:i", (req, res) => {
-    const id = req.params.i;
-    blogs.splice(id, 1);
-    res.redirect("/blogList")
+app.post('/delete/:i', (req, res) => {
+  const id = req.params.i
+  blogs.splice(id, 1)
+  res.redirect('/blogList')
 })
 
 module.exports = { app }
--- blogging-site/index.js
+++ blogging-site/index.js
@@ -1,6 +1,6 @@
-const { app } = require("./app")
-const PORT = 5000;
+const { app } = require('./app')
+const PORT = 5000
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`)
 })
